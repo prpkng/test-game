fcfae7759b932f2f25e60873830ad154627151af
Purpkin <guiejo2020@gmail.com>
2025-02-11 21:57:07 -0300

    Revert "Coroutine state"
    This reverts commit 0d874e3eebf358f5189c23f5d59bf86b2d0a3682.

--- src/game/tools/fsm/FSM.hx
+++ src/game/tools/fsm/FSM.hx
@@ -1,6 +1,5 @@
 package tools.fsm;
 
-import coroutine.CoroutineRunner;
 import haxe.Exception;
 import haxe.ds.Map;
 import haxe.macro.Expr;
@@ -35,7 +34,7 @@ interface IState {
 	 */
 	public function setParent(parent:FSM):Void;
 
-	public function canExit():Bool;
+    public function canExit():Bool;
 }
 
 /**
@@ -45,16 +44,16 @@ class StateBase implements IState {
 	public var _fsm:FSM;
 
 	public var fsm(get, never):FSM;
-	public var hasExitTime = false;
-	public var canSafelyExit = false;
+    public var hasExitTime = false;
+    public var canSafelyExit = false;
 
-	public function new(hasExitTime:Bool = false) {
-		this.hasExitTime = hasExitTime;
-	}
+    public function new(hasExitTime:Bool = false) {
+        this.hasExitTime = hasExitTime;
+    }
 
-	public function canExit():Bool {
-		return hasExitTime ? canSafelyExit : true;
-	}
+    public function canExit():Bool {
+        return hasExitTime ? canSafelyExit : true;
+    }
 
 	public function get_fsm():FSM {
 		return _fsm;
@@ -67,40 +66,10 @@ class StateBase implements IState {
 	public function onEnter() {}
 
 	public function onExit() {
-		canSafelyExit = false;
-	}
+        canSafelyExit = false;
+    }
 
 	public function update(tmod:Float) {}
-}
-
-class CoState extends StateBase {
-	var runner:CoroutineRunner;
-	var routine:Routine;
-
-	public function new(routine:coroutine.Routine) {
-		runner = new CoroutineRunner();
-		this.routine = routine;
-	}
-
-	public override function onEnter() {
-		super.onEnter();
-		runner.startCoroutine(routine);
-	}
-
-	public override function onExit() {
-		super.onExit();
-		runner.stopCoroutine(routine);
-	}
-
-	override function update(tmod:Float) {
-		super.update(tmod);
-
-		var processor = CoroutineProcessor.of(runner);
-		processor.updateEnterFrame();
-		processor.updateTimer(haxe.Timer.stamp());
-		processor.updateExitFrame();
-		
-	}
 }
 
 class State extends StateBase {
@@ -112,7 +81,7 @@ class State extends StateBase {
 	 */
 	public function new(hasExitTime:Bool = false, ?onEnter:Null<() -> Void>, ?update:Null<(tmod:Float) -> Void>, ?onExit:Null<() -> Void>) {
 		super(hasExitTime);
-		if (onEnter != null)
+        if (onEnter != null)
 			this._onEnter = onEnter;
 		if (update != null)
 			this._update = update;
@@ -129,7 +98,7 @@ class State extends StateBase {
 	dynamic function _onExit() {}
 
 	public override function onExit() {
-		super.onExit();
+        super.onExit();
 		_onExit();
 	}
 
@@ -144,82 +113,80 @@ class FSM implements IState {
 	var parent:Null<FSM>;
 
 	var states:Map<String, IState> = new Map();
-	var currentStateName:Null<String>;
+    var currentStateName:Null<String>;
+    
+    var currentState(get, never):IState;
+    function get_currentState():IState {
+        return states.get(currentStateName);
+    }
 
-	var currentState(get, never):IState;
-
-	function get_currentState():IState {
-		return states.get(currentStateName);
-	}
-
-	var requestedTransition:Null<String>;
-
+    var requestedTransition:Null<String>;
+    
 	public var startState:String;
 
 	public var fsm(get, never):FSM;
-	public var hasExitTime = false;
-	public var canSafelyExit = false;
+    public var hasExitTime = false;
+    public var canSafelyExit = false;
 
 	public function get_fsm():FSM {
 		return parent ?? this;
 	}
 
 	public function new(?initialStates:Null<Map<String, IState>>, hasExitTime = false) {
-		this.hasExitTime = hasExitTime;
+        this.hasExitTime = hasExitTime;
 		for (id => state in initialStates) {
 			states.set(id, state);
 			state.setParent(this);
-			startState = id;
+            startState = id;
 		}
 	}
 
 	public function setStartState(idx:String) {
 		if (!states.exists(idx)) {
-			throw new Exception('No state found in FSM named: ${idx}');
-		}
-		startState = idx;
-		onEnter();
+            throw new Exception('No state found in FSM named: ${idx}');
+        }
+        startState = idx;
+        onEnter();
 	}
 
-	public function requestTransition(to:String) {
+    public function requestTransition(to: String) {
 		if (!states.exists(to)) {
-			throw new Exception('No state found in FSM named: ${to}');
-		}
-		requestedTransition = to;
-	}
+            throw new Exception('No state found in FSM named: ${to}');
+        }
+        requestedTransition = to;
+    } 
 
-	public function canExit():Bool {
-		return hasExitTime ? canSafelyExit : true;
-	}
+    public function canExit():Bool {
+        return hasExitTime ? canSafelyExit : true;
+    }
 
 	public function setParent(fsm:FSM) {
 		parent = fsm;
 	}
 
 	public function onEnter() {
-		if (currentStateName == null) {
-			if (startState == null)
-				return;
-			requestTransition(startState);
-			return;
-		}
-		currentState.onEnter();
-	}
+        if (currentStateName == null) {
+            if (startState == null) return;
+            requestTransition(startState);
+            return;
+        }
+        currentState.onEnter();
+    }
 
 	public function onExit() {
-		currentState.onExit();
-	}
+        currentState.onExit();
+    }
 
 	public function update(tmod:Float) {
-		if (requestedTransition != null) {
-			if (states.exists(currentStateName))
-				currentState.onExit();
-			currentStateName = requestedTransition;
-			requestedTransition = null;
-			currentState.onEnter();
-		}
-		if (states.exists(currentStateName)) {
-			currentState.update(tmod);
-		}
-	}
+        if (requestedTransition != null) {
+            if (states.exists(currentStateName))
+                currentState.onExit();
+            currentStateName = requestedTransition;
+            requestedTransition = null;
+            currentState.onEnter();
+        }
+        if (states.exists(currentStateName)) {
+            currentState.update(tmod);
+        }
+    }
 }
